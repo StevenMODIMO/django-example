63961a5e73a64e780b49ebbfdcf8054ba72c2345
StevenMODIMO <stevepetes26@gmail.com>
2024-01-20 12:16:58 +0300

    final

--- members/views.py
+++ members/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 # Create your views here.
 def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('members.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&(4cq-85!-*=_o4too-55!5_@i_1^x+g*r&6c@3qkggd#^%%b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = './static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
